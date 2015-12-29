8d80a489902d8cf73e1b229be212f9ef74a2f2bd
Stephen L <lrq3000@gmail.com>
2015-12-29 16:21:08 +0100

    add ipython/jupyter notebook support
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -3,7 +3,9 @@ from ._tqdm import trange
 from ._tqdm_gui import tqdm_gui
 from ._tqdm_gui import tgrange
 from ._tqdm_pandas import tqdm_pandas
+from ._tqdm_notebook import tqdm_notebook
+from ._tqdm_notebook import tnrange
 from ._version import __version__  # NOQA
 
 __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange', 'tqdm_pandas',
-           '__version__']
+           'tqdm_notebook', 'tnrange', '__version__']
